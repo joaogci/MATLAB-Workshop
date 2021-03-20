b00876147c635d41a702107a2e37b5ae51a95520
GustavoGil13 <gustavogillmorais@gmail.com>
2021-03-20 19:34:57 +0000

    Melhorei o português

--- ex_2_sol.m
+++ ex_2_sol.m
@@ -1,34 +1,34 @@
-clc
+% Exercicios 2ª sessão Workshop de MATLAB
 clear
-close all
 %% Plots
 %{
-    Como vimos na sessão anterior, no MATLAB podemos fazer gráficos através de plots. 
-    Podemos também customizar os plots de forma a vermos detalhadamente certas secções do gráfico.
-    Podemos também adicionar legendas aos eixos e também dar legendas caso
-    haja muitos gráficos representados.
+    Como vimos na sessão anterior, podemos fazer gráficos através de plots. 
+    Podemos também customizar os plots de forma a vermos detalhadamente
+    certas secções do gráfico, adicionar legendas aos eixos, dar legendas 
+    caso haja muitos gráficos representados, etc..
 
-    Faça o plot das funções f(x)=4*cos(5*x), g(x)=2*sen(2*x + pi/4), 
-    h(x)=cos(x)*sen(x) e j(x)=2*cos(x)-3*sen(x), em que x é o vetor que começa em -2pi e vai
-    até 2*pi com um passo de 0,1.
+    Faça o plot das funções 
+    f(x) = 4*cos(5*x)
+    g(x) = 2*sen(2*x + pi/4), 
+    h(x) = cos(x)*sen(x)
+    j(x) = 2*cos(x)-3*sen(x)
 
-    
+    onde x é o vetor que começa em -2pi e vai até 2*pi com um passo de 0.1.
 %}
 
-x=-2*pi:0.1:2*pi;
-f=4*cos(5*x);
-g=2*sin(2*x + pi/4);
-h=cos(x).*sin(x);
-j=2*cos(x)-3*sin(x);
+x = -2*pi:0.1:2*pi;
 
-
+f = 4*cos(5*x);
+g = 2*sin(2*x + pi/4);
+h = cos(x).*sin(x);
+j = 2*cos(x)-3*sin(x);
 
 plot(x,f,'-b',x,g,'.r',x,h,'*g',x,j,'ok')
 close
 
 %{
-    Separe os gráficos das funções, adicione título 
-    e costumize de forma diferente cada um.
+    Separe os gráficos das funções descritas a cima, adicione título 
+    e costumize cada um de forma diferente.
 %}
 
 subplot(2,2,1)
@@ -56,7 +56,7 @@ ylabel('g(x)')
 xlabel('x')
 
 
-%Repita o exercicio anterior atribuindo um gráfico a cada janela.
+%   Repita o exercicio anterior atribuindo uma janela a cada gráfico.
 
 close
 
@@ -87,19 +87,23 @@ xlabel('x')
 close all
 
 %{
-    Como já pode ter reparado existe um ficheiro plot3d.mat neste
-    diretorio. Tendo em conta que este ficheiro é composto por 3 variáveis (X,Y,Vnew). 
-    Utilize a função load para descarregar os valores para a sua workspace.
+    Como já pode ter reparado, existe um ficheiro plot3d.mat neste
+    diretorio. Tendo em conta que este ficheiro é composto por 3 variáveis 
+    (X,Y,Vnew). Utilize a função load para as descarregar para a sua 
+    workspace.
 %}
 
 load("plot3d","X","Y","Vnew");
 
 %{
-    Utilize a função plot3 e mesh para visualizar os seus valores.
-    Para vizualizar melhor com a função plot3 passe como argumento '.'
+    Utilize as funções plot3 e mesh para visualizar os seus valores.
+    Nota: é recomendado que passe como argumento '.' à função plot3.
 %}
 
 figure(1)
 plot3(X,Y,Vnew,'.')
 figure(2)
 mesh(X,Y,Vnew)
+
+%% Controlo de fluxo
+
